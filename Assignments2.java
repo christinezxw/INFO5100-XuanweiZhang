713fa57c8c33fefdb376e19c80e6dd673c836aed
christine zhang <zhang.xuanw@husky.neu.edu>
2019-09-16 14:44:12 -0700

    submit assignments 2

---
+++ Assignments2.java
@@ -0,0 +1,153 @@
+package assignments2;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*; 
+
+public class Assignments2 {
+    /*
+        This method should return the sum of salaries of employees having salary greater than 5000
+        Note: Employee array is passed, not employee
+     */
+    public static double salaryGreaterThanFiveThousand(Employee[] employees) {
+        // @TODO
+        double sum = 0.0;
+        for (Employee employee : employees) {
+            if (employee.getSalary() > 5000) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    /*
+        This method should print either "Fizz", "Buzz" or "FizzBuzz"
+        "Fizz" - if id of employee is divisible by 3
+        "Buzz" - if id of employee is divisible by 5
+        "FizzBuzz" - if id of employee is divisible by both 3 and 5
+     */
+    public static void fizzBuzz(Employee employee) {
+        // @TODO
+        if (employee.getId() % 3 == 0 && employee.getId() % 5 == 0) {
+            System.out.println("FizzBuzz");
+        }else if(employee.getId() % 3 == 0) {
+            System.out.println("Fizz");
+        }else if(employee.getId() % 5 == 0) {
+            System.out.println("Buzz");
+        }
+    }
+
+    /*
+        This method should calculate tax for an employee in dollars
+        If salary of employee is less than or equal to 2500, tax should be 10%
+        If salary of employee is greater than 2500 but less than or equal to 5000, tax should be 25%
+        If salary of employee is greater than 5000, tax should be 35%
+    */
+    public static double calculateTax(Employee employee) {
+        // @TODO
+        if (employee.getSalary() > 5000) {
+            return employee.getSalary() * 0.35;
+        }else if(employee.getSalary() > 2500) {
+            return employee.getSalary() * 0.25;
+        }else {
+            return employee.getSalary() * 0.1;
+        }
+    }
+
+    /*
+        We are given two employee objects.
+        Implement this method to swap salaries of employees
+    */
+    public static void swap(Employee firstEmployee, Employee secondEmployee) {
+        // @TODO
+        double temp = firstEmployee.getSalary();
+        firstEmployee.setSalary(secondEmployee.getSalary());
+        secondEmployee.setSalary(temp);
+    }
+
+    /*
+        Return number of employees whose age is greater than 50
+        Note: Employee array is passed, not employee
+     */
+    public static int employeesAgeGreaterThan50(Employee[] employees) {
+        // @TODO
+        int sum = 0;
+        for (Employee employee : employees) {
+            if (employee.getAge() > 50) {
+                sum ++;
+            }
+        }
+        return sum;
+    }
+
+    /*
+        Implement this method to convert String[] to employees array.
+        Ex: String[] employees = new String[]{"1,John,24,7500", "2,Hail,28,7899.90"};
+        This String array of length 2 contains 2 employees in form of string, where
+        id = 1
+        firstName=John
+        age=24
+        salary=7500
+        convert each string to employee object.
+        Hint: Use String methods. Refer : https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+     */
+    public static Employee[] createEmployees(String[] employeesStr) {
+        Employee[] employees = new Employee[employeesStr.length];
+        // @TODO
+        for (int i = 0; i < employeesStr.length; i ++){
+            String[] initInfo = employeesStr[i].split(",");
+            employees[i] = new Employee(Integer.parseInt(initInfo[0]), initInfo[1], Integer.parseInt(initInfo[2]), Double.parseDouble(initInfo[3]));
+        }
+        return employees;
+    }
+    
+    /*
+        Implement this method to reverse firstName of employee.
+        Ex. Before: employee.getFirstName() -> "John"
+        After : employee.getFirstName() -> "nhoJ"
+     */
+    public static void reverseFirstName(Employee employee) {
+        // @TODO
+        StringBuilder str = new StringBuilder();
+        str.append(employee.getFirstName());
+        str = str.reverse();
+        employee.setFirstName(str.toString());
+    }
+
+    /*
+        Print "true" if employee's first name contain one or more digits
+        Print "false" if employee's first name doesn't contain any digit
+        Ex: employee.getFirstName() -> "ha8l" == true
+        employee.getFirstName() -> "hail" == false
+     */
+    public void isContainDigit(Employee employee) {
+        // @TODO
+        for (int i = 0; i < employee.getFirstName().length(); i ++) {
+            if (Character.isDigit(employee.getFirstName().charAt(i))) {
+                System.out.println("True");
+            }
+        }
+        System.out.println("False");
+    }
+    
+    public static void main(String[] args) {
+        String[] employees = new String[]{"3,John,24,3500", "5,Hail,55,7899.90"};
+        Employee[] empList = createEmployees(employees);
+        for (Employee e : empList) {
+            System.out.println(e.getId() + " " + e.getFirstName() + " " + e.getAge() + " " + e.getSalary());
+            fizzBuzz(e);
+            System.out.println("calculate Tax = " + calculateTax(e));
+            reverseFirstName(e);
+            System.out.println("reversed firstname: " + e.getFirstName());
+        }
+        System.out.println("salaryGreaterThanFiveThousand: " + salaryGreaterThanFiveThousand(empList));
+        System.out.println("employeesAgeGreaterThan50: " + employeesAgeGreaterThan50(empList));
+        
+        swap(empList[0], empList[1]);
+        System.out.println("swapping salary: ");
+        for (Employee e : empList) {
+            System.out.println("salary of "+e.getFirstName()+ " = " + e.getSalary());
+        }
+    }
+}
+
---
+++ AssignmentsX.java
@@ -0,0 +1,185 @@
+package com.itranswarp.sample;
+
+import java.util.Objects;
+
+public class Assignments2 {
+    private static final String ORGANISATION = "Northeastern";
+    private static final String[] STRS = new String[] {
+            //firstname,lastname,age,salary,email,designation
+            "John,Doe,24,5400,johndoe@gmail.com,Engineer",
+            "Sara,Jones,26,6300,sarajones@gmail.com,TechnicalRecruiter",
+            "Noah,Trevor,32,5300,noahtrevor@gmail.com,Writer",
+            "Jill,Meyer,34,6800,jillmeyer@gmail.com,Manager",
+            "Chris,Strat,36,7600,chrisstrat@gmail.com,Director",
+            "Ching,Chi,25,4000,chingchi@gmail.com,Developer",
+            "Yue,Hui,28,4900,yuehui@gmail.com,Tester",
+            "Amit,Agarwal,27,5700,yuehui@gmail.com,SeniorDeveloper",
+            "Tianyu,Wang,26,4500,tianyuwang@gmail.com,Researcher",
+            "Ram,Menon,29,5700,rammenon@gmail.com,DataScientist"
+    };
+
+    private final Employee[] employees = new Employee[10];
+
+    class Employee implements Comparable<Employee> {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private int salary;
+        private String email;
+        private String designation;
+        private String org;
+
+        Employee(String firstName, String lastName, int age, int salary, String email, String designation, String org) {
+            // @TODO
+            // initialize all members here
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+            this.email = email;
+            this.designation = designation;
+            this.org = org;
+        }
+
+        // generate getter setters for all Employee members
+
+        @Override
+        public int compareTo(Employee o) {
+            // @TODO
+            // compare employee by their salary, if salaries are equal, compare by age
+            if (o instanceof Employee) {
+                Employee emp = (Employee) o;
+                int i = this.salary.compareTo(emp.salary);
+                
+                if (i == 0) {
+                    return this.age.compareTo(emp.age);
+                }else {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Employee other = (Employee) obj;
+            if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+                return false;
+            if (age != other.age)
+                return false;
+            if (designation == null) {
+                if (other.designation != null)
+                    return false;
+            } else if (!designation.equals(other.designation))
+                return false;
+            if (email == null) {
+                if (other.email != null)
+                    return false;
+            } else if (!email.equals(other.email))
+                return false;
+            if (firstName == null) {
+                if (other.firstName != null)
+                    return false;
+            } else if (!firstName.equals(other.firstName))
+                return false;
+            if (lastName == null) {
+                if (other.lastName != null)
+                    return false;
+            } else if (!lastName.equals(other.lastName))
+                return false;
+            if (org == null) {
+                if (other.org != null)
+                    return false;
+            } else if (!org.equals(other.org))
+                return false;
+            if (salary != other.salary)
+                return false;
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + getEnclosingInstance().hashCode();
+            result = prime * result + age;
+            result = prime * result + ((designation == null) ? 0 : designation.hashCode());
+            result = prime * result + ((email == null) ? 0 : email.hashCode());
+            result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+            result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+            result = prime * result + ((org == null) ? 0 : org.hashCode());
+            result = prime * result + salary;
+            return result;
+        }
+
+        private Assignments2 getEnclosingInstance() {
+            return Assignments2.this;
+        }
+    }//class define ends
+
+    public void initializeEmployees() {
+        // @TODO
+        // use STRS to create employee objects and store them in employees array
+        for (int i = 0; i < employees.length; i ++){
+            String[] initInfo = STRS[i].split(",");
+            employees[i] = new Employee(initInfo[0], initInfo[1], Integer.parseInt(initInfo[2]), Integer.parseInt(initInfo[3]), initInfo[4], initInfo[5], ORGANISATION);
+        }
+        
+    }
+
+    public int sumOfSalariesGreaterThanFiveThousands() {
+        // @TODO
+        // calculate the sum of salaries of employees having salary greater than 5000
+        int sum = 0;
+        for (Employee e : employees) {
+            if (e.salary > 5000) {
+                sum += e.salary;
+            }
+        }
+        return sum;
+    }
+    
+    public static void swap(Employee e1, Employee e2) {
+        int temp = e1.salary;
+        e1.salary = e2.salary;
+        e2.salary = temp;
+    }
+
+    public void swapSalaries() {
+        // @TODO
+        // swap salary of even numbered employee with odd numbered employee
+        // swap salary of employees[0] with employees[1], employees[2] with employees[3] and so on..
+        for (int i = 0; i < employees.length; i ++) {
+            if (i % 2 == 0) {
+                swap(employees[i], employees[i + 1]);
+            }
+        }
+    }
+
+    public void customSort() {
+        // @TODO
+        // sort employees array on the basis of firstName and print
+        
+        for(Employee employee : employees) {
+            System.out.println(employee.firstName);
+        }
+
+        // @TODO
+        // sort employees array on the basis of age and print
+
+        for(Employee employee : employees) {
+            System.out.println(employee.firstName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Assignment2 assignment2 = new Assignment2();
+        // you can verify/test your code here
+    }
+}
